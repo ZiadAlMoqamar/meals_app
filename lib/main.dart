7794fe1212c54fba0c7792a433c9bd85805b9c26
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2021-12-15 20:47:52 +0200

    adding static route name

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meals App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.blue,
-        ).copyWith(
-          secondary: Colors.green,
-        ),
-        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-          bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-          headline6: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'RobotoCondensed'
-          )
-        )
-      ),
-      home: const CategoriesScreen(
-        dummyCategories: dummyCategories,
-      ),
-      routes:  {
-        '/category-meals': (ctx)=> CategoryMealsScreen(),
-      }
-    );
+        title: 'Meals App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: Colors.blue,
+            ).copyWith(
+              secondary: Colors.green,
+            ),
+            canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1:
+                    const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                bodyText2:
+                    const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                headline6: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'RobotoCondensed'))),
+        initialRoute: '/',
+        routes: {
+          '/': (ctx) =>
+              const CategoriesScreen(dummyCategories: dummyCategories),
+          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        });
   }
 }
